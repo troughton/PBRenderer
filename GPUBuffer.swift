f8ee5035b41ec7d496f6144dc8eac438190d94b9
Thomas Roughton <t.roughton@me.com>
2016-04-29 11:55:24 +1200

    Initial commit.

---
+++ GPUBuffer.swift
@@ -0,0 +1,137 @@
+//
+//  GPUBuffer.swift
+//  PBRenderer
+//
+//  Created by Thomas Roughton on 28/04/16.
+//
+//
+
+import Foundation
+import SGLOpenGL
+
+public enum GPUBufferAccessFrequency {
+    /// The data store contents will be modified once and used at most a few times.
+    case Stream
+    /// The data store contents will be modified once and used many times.
+    case Static
+    /// The data store contents will be modified repeatedly and used many times.
+    case Dynamic
+}
+
+public enum GPUBufferAccessType {
+    /// The data store contents are modified by the application, and used as the source for GL drawing and image specification commands.
+    case Draw
+    /// The data store contents are modified by reading data from the GL, and used to return that data when queried by the application.
+    case Read
+    /// The data store contents are modified by reading data from the GL, and used as the source for GL drawing and image specification commands.
+    case Copy
+}
+
+public final class GPUBuffer<T> {
+    
+    private let bufferToBindTo = GL_UNIFORM_BUFFER
+    
+    public let capacity : Int
+    
+    public let contents : UnsafeMutableBufferPointer<T>
+    private let _glBuffer : GLuint
+    
+    public var capacityInBytes : Int {
+        return self.capacity * sizeof(T.Type)
+    }
+    
+    init(capacity: Int, data: [T]? = nil, accessFrequency: GPUBufferAccessFrequency, accessType: GPUBufferAccessType) {
+        self.capacity = capacity
+        
+        let baseAddress = UnsafeMutablePointer<T>(calloc(sizeof(T), capacity))
+        self.contents = UnsafeMutableBufferPointer(start: baseAddress, count: capacity)
+        
+        
+        var uniformBlockRef = GLuint(0)
+        glGenBuffers(1, &uniformBlockRef);
+        glBindBuffer(self.bufferToBindTo, uniformBlockRef);
+        _glBuffer = uniformBlockRef
+        
+        let usage : GLint
+        
+        switch (accessFrequency, accessType) {
+        case (.Stream, .Draw):
+            usage = GL_STREAM_DRAW
+        case (.Static, .Draw):
+            usage = GL_STATIC_DRAW
+        case (.Dynamic, .Draw):
+            usage = GL_DYNAMIC_DRAW
+        case (.Stream, .Read):
+            usage = GL_STREAM_READ
+        case (.Static, .Read):
+            usage = GL_STATIC_READ
+        case (.Dynamic, .Read):
+            usage = GL_DYNAMIC_READ
+        case (.Stream, .Copy):
+            usage = GL_STREAM_COPY
+        case (.Static, .Copy):
+            usage = GL_STATIC_COPY
+        case (.Dynamic, .Copy):
+            usage = GL_DYNAMIC_COPY
+        }
+        
+        glBufferData(self.bufferToBindTo, self.capacityInBytes, data, usage);
+        
+        glBindBuffer(self.bufferToBindTo, 0)
+    }
+    
+    subscript(_ idx: Int) -> T {
+        get {
+            return contents[idx]
+        }
+        set(newValue) {
+            self.contents[idx] = newValue
+        }
+    }
+    
+    subscript(_ range: Range<Int>) -> [T] {
+        get {
+            return [T](UnsafeMutableBufferPointer(start: self.contents.baseAddress?.advanced(by: range.startIndex), count: range.count))
+        }
+        set(newValue) {
+            assert(range.count == newValue.count)
+            
+            newValue.withUnsafeBufferPointer { (toCopy) -> Void in
+                let destination = self.contents.baseAddress?.advanced(by: range.startIndex)
+                memcpy(destination, toCopy.baseAddress, range.count * sizeof(T))
+            }
+        }
+    }
+    
+    deinit {
+        free(contents.baseAddress)
+        
+        var glBuffer = _glBuffer
+        glDeleteBuffers(1, &glBuffer)
+    }
+    
+    public func didModify() {
+        self.didModifyRange(0..<self.capacity)
+    }
+    
+    public func didModifyRange(_ range: Range<Int>) {
+        glBindBuffer(self.bufferToBindTo, _glBuffer)
+        glBufferSubData(target: self.bufferToBindTo, offset: range.startIndex, size: range.count, data: self.contents.baseAddress?.advanced(by: range.startIndex))
+        glBindBuffer(self.bufferToBindTo, 0)
+    }
+    
+    public func updateFromGPU() {
+        self.updateFromGPU(range: 0..<self.capacity)
+    }
+    
+    public func updateFromGPU(range: Range<Int>) {
+        glBindBuffer(self.bufferToBindTo, _glBuffer)
+        glGetBufferSubData(self.bufferToBindTo, range.startIndex, range.count, contents.baseAddress?.advanced(by: range.startIndex))
+        glBindBuffer(self.bufferToBindTo, 0)
+    }
+    
+    public func bindToUniformBlockIndex(_ index: Int, elementOffset: Int = 0) {
+        let offset = elementOffset * sizeof(T)
+        glBindBufferRange(GL_UNIFORM_BUFFER, GLuint(index), _glBuffer, offset, self.capacityInBytes - offset)
+    }
+}
---
+++ Shader.swift
@@ -0,0 +1,216 @@
+//
+//  Shader.swift
+//  PBRenderer
+//
+//  Created by Thomas Roughton on 28/04/16.
+//
+//
+
+import Foundation
+import SGLOpenGL
+import SGLMath
+
+protocol ShaderProperty {
+    var name: String { get }
+}
+
+class Shader {
+    
+    private var uniformMappings = [String : GLint]()
+    
+    private let _glProgramRef : GLuint
+    
+    init(withVertexShader vertexShader: String, fragmentShader: String) {
+        let shaders = [Shader.createShader(type: GL_VERTEX_SHADER, text: vertexShader), Shader.createShader(type: GL_FRAGMENT_SHADER, text: fragmentShader)]
+        _glProgramRef = Shader.createProgram(shaderList: shaders)
+    }
+    
+    func useProgram() {
+
+        glUseProgram(_glProgramRef)
+    }
+    
+    func endUseProgram() {
+        glUseProgram(0)
+    }
+    
+    private func uniformLocation(forProperty property: ShaderProperty) -> GLint? {
+        if let location = self.uniformMappings[property.name] {
+            return location
+        }
+        
+        var location = glGetUniformLocation(_glProgramRef, property.name)
+        if location == 0 {
+            location = GLint(glGetUniformBlockIndex(_glProgramRef, property.name));
+        }
+        if location == 0 {
+            print("Warning: uniform for property \(property) was not found.")
+            return nil
+        } else {
+            self.uniformMappings[property.name] = location
+            return location
+        }
+    }
+    
+    func setUniformBlockBindingPoints(forProperties properties: [ShaderProperty?]) {
+        for (index, property) in properties.enumerated() where property != nil {
+            guard let uniformBlockIndex = self.uniformLocation(forProperty: property!) else { continue }
+            glUniformBlockBinding(_glProgramRef, GLuint(uniformBlockIndex), GLuint(index))
+        }
+    }
+    
+}
+
+extension Shader {
+    
+    /**
+    * Creates and links a shader program using the specified OpenGL shader objects.
+    */
+    private static func createProgram(shaderList: [GLuint]) -> GLuint {
+    
+        let program = glCreateProgram();
+    
+        for shader in shaderList {
+            glAttachShader(program, shader);
+        }
+    
+        glLinkProgram(program);
+        
+        var status : GLint = 0
+        glGetProgramiv(program, GL_LINK_STATUS, &status)
+        
+        if (status == GL_FALSE) {
+            var infoLogLength : GLint = 0
+            glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
+    
+            let error = UnsafeMutablePointer<GLchar>(calloc(sizeof(GLchar), Int(infoLogLength)))
+            
+            glGetProgramInfoLog(program, infoLogLength, nil, error);
+            
+            let errorString = String(CString: error, encoding: NSUTF8StringEncoding)
+            
+            free(error)
+            
+            print("Linker failure: \(errorString)");
+        }
+    
+        for shader in shaderList {
+            glDetachShader(program, shader);
+        }
+    
+        return program;
+    }
+    
+    /**
+    * Creates and compiles a shader from the given text.
+    * @param shaderType The type of the shader. Any of GL_VERTEX_SHADER, GL_GEOMETRY_SHADER, or GL_FRAGMENT_SHADER.
+    */
+    private static func createShader(type: GLenum, text: String) -> GLuint {
+        let shader = glCreateShader(type);
+        let cString = text.cString(using: NSUTF8StringEncoding)!
+        let baseAddress = cString.withUnsafeBufferPointer { (shaderText) -> UnsafePointer<GLchar>! in
+            return shaderText.baseAddress
+        }
+        let lengths = [GLint(cString.count)]
+        let shaderTexts = [baseAddress]
+        
+        glShaderSource(shader, 1, shaderTexts, lengths);
+        
+        glCompileShader(shader);
+    
+        var status : GLint = 0
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+        if (status == GL_FALSE) {
+            var infoLogLength : GLint = 0
+            glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+    
+            let error = UnsafeMutablePointer<GLchar>(calloc(sizeof(GLchar), Int(infoLogLength)))
+            
+            glGetShaderInfoLog(shader, infoLogLength, nil, error);
+            
+            let errorString = String(CString: error, encoding: NSUTF8StringEncoding)
+            
+            free(error)
+    
+            let strShaderType : String;
+            switch (type) {
+                case GL_VERTEX_SHADER: strShaderType = "vertex";
+                case GL_GEOMETRY_SHADER: strShaderType = "geometry";
+                case GL_FRAGMENT_SHADER: strShaderType = "fragment";
+                default: strShaderType = "";
+            }
+            print("Compile failure in \(strShaderType) shader:\n\(errorString)")
+        }
+        return shader;
+    }
+}
+
+/** Matrix setting. */
+extension Shader {
+    
+    func setMatrix(_ matrix : mat4, forProperty property: ShaderProperty) {
+        guard let uniformRef = self.uniformLocation(forProperty: property) else {
+            return
+        }
+        
+        var matrix = matrix
+        withUnsafePointer(&matrix) { (matrixPtr) -> Void in
+            glUniformMatrix4fv(uniformRef, 1, false, UnsafePointer(matrixPtr))
+        }
+    }
+    
+    func setMatrix(_ matrix : mat3, forProperty property: ShaderProperty) {
+        guard let uniformRef = self.uniformLocation(forProperty: property) else {
+            return
+        }
+        
+
+        let matrixArray : [Float] = [matrix[0][0], matrix[0][1], matrix[0][2], matrix[1][0], matrix[1][1], matrix[1][2], matrix[2][0], matrix[2][1], matrix[2][2]]
+        
+        glUniformMatrix3fv(uniformRef, 1, false, matrixArray)
+    }
+}
+
+/** Uniform setting. */
+extension Shader {
+    
+    func setUniform(_ values : Float..., forProperty property: ShaderProperty) {
+        guard let uniformRef = self.uniformLocation(forProperty: property) else {
+            return
+        }
+        
+        switch values.count {
+        case 1:
+            glUniform1f(uniformRef, values[0])
+        case 2:
+            glUniform2f(uniformRef, values[0], values[1])
+        case 3:
+            glUniform3f(uniformRef, values[0], values[1], values[2])
+        case 4:
+            glUniform4f(uniformRef, values[0], values[1], values[2], values[3])
+        default:
+            assertionFailure("There is no uniform mapping for the values \(values) of length \(values.count)")
+            break;
+        }
+    }
+    
+    func setUniform(_ values : GLint..., forProperty property: ShaderProperty) {
+        guard let uniformRef = self.uniformLocation(forProperty: property) else {
+            return
+        }
+        
+        switch values.count {
+        case 1:
+            glUniform1i(uniformRef, values[0])
+        case 2:
+            glUniform2i(uniformRef, values[0], values[1])
+        case 3:
+            glUniform3i(uniformRef, values[0], values[1], values[2])
+        case 4:
+            glUniform4i(uniformRef, values[0], values[1], values[2], values[3])
+        default:
+            assertionFailure("There is no uniform mapping for the values \(values) of length \(values.count)")
+            break;
+        }
+    }
+}
