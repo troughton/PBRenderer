bab7dd186350484084b3c47777dc05e8f842ace4
Thomas Roughton <t.roughton@me.com>
2016-06-06 22:15:03 +1200

    Corrections to light probe evaluation code.

--- Resources/Shaders/OpenGL/LightProbe.glsl
+++ Resources/Shaders/OpenGL/LightProbe.glsl
@@ -64,88 +64,91 @@ vec4 evaluateIBLSpecular(vec3 N, vec3 R, float NdotV, float linearRoughness, flo
 
 ////Parallax correction for reflections in a local light probe.
 ////https://seblagarde.wordpress.com/2012/09/29/image-based-lighting-approaches-and-parallax-corrected-cubemap/
-//vec3 reflectionDirectionForLocalLightProbe(vec4 worldSpacePosition, vec3 worldSpaceReflectionDirection, mat4 cubeMapVolumeWorldToLocal, vec3 cubeMapPositionWS, float linearRoughness,
-//                                           out float distIntersectionToShadedPoint,
-//                                           out float distIntersectionToProbeCentre) {
-//    vec3 rayLS = mat3(cubeMapVolumeWorldToLocal) * worldSpaceReflectionDirection;
-//    vec3 positionLS = (cubeMapVolumeWorldToLocal * worldSpacePosition).xyz;
-//    
-//    vec3 unitary = vec3(1.f);
-//    vec3 firstPlaneIntersect = (unitary - positionLS) / rayLS;
-//    vec3 secondPlaneIntersect = (-unitary - positionLS) / rayLS;
-//    
-//    vec3 furthestPlane = max(firstPlaneIntersect, secondPlaneIntersect);
-//    float dist = min(furthestPlane.x, min(furthestPlane.y, furthestPlane.z));
-//    
-//    vec3 intersectPositionWS = worldSpacePosition + worldSpaceReflectionDirection * dist;
-//    vec3 reflectionDirectionWS = intersectPositionWS - cubeMapPositionWS;
-//    
-//    distIntersectionToShadedPoint = dist;
-//    distIntersectionToProbeCentre = distance(intersectPositionWS, cubeMapPositionWS);
-//    
-//    return mix(reflectionDirectionWS, worldSpaceReflectionDirection, linearRoughness);
-//}
-//
-//float computeDistanceBasedLinearRoughness(
-//                                   float distIntersectionToShadedPoint,
-//                                   float distIntersectionToProbeCenter,
-//                                   float linearRoughness) {
-//    // To avoid artifacts we clamp to the original linearRoughness
-//    // which introduces an acceptable bias and allows conservation
-//    // of mirror reflection behavior for a smooth surface.
-//    float newLinearRoughness = clamp(distIntersectionToShadedPoint /
-//                                       distIntersectionToProbeCenter * linearRoughness, 0, linearRoughness);
-//    return mix(newLinearRoughness, linearRoughness, linearRoughness);
-//}
-//
-//struct LightProbe {
-//    mat4 boundingVolumeWorldToLocal;
-//    vec4 cubeMapPosition;
-//    int isEnvironmentMap;
-//    int padding;
-//    int padding2;
-//    int padding3;
-//}
-//
-//void evaluateLightProbe(vec3 N, vec3 V, float NdotV, vec3 R, MaterialRenderingData material, LightProbe lightProbe) {
-//    
-//    if (useEnvironmentMap) {
-//        
-//        vec3 result = evaluateIBLDiffuse(N, V, NdotV, material.roughness) * material.albedo;
-//        result += evaluateIBLSpecular(N, R, NdotV, material.linearRoughness, material.roughness, material.f0, material.f90);
-//        return result;
-//        
-//    } else {
-//        return vec3(0);
-//    }
-//}
-//
-//
-//
-//#define MaxLightProbeCount 64
-//
-//layout(std140) uniform LightProbe lightProbes[MaxLightProbeCount];
-//
-//vec3 evaluateIBL(vec3 worldSpacePosition, vec3 N, vec3 V, float NdotV, vec3 R, MaterialRenderingData material, int lightProbeIndices[MaxLightProbeCount + 1]) {
-//    vec4 total = vec4(0);
-//    
-//    int lightProbeCount = lightProbeIndices[0];
-//    // Medium range reflections
-//    for (int i = 0; i < lightProbeCount && total.a < 1.0; i++) {
-//        LightProbe probe = lightProbes[i + 1];
-//        
-//        vec4 evaluationResult = evaluateLightProbe(N, V, NdotV, R, material, probe);
-//        float alpha = saturate(
-//        
-//    }
-//    
-//    While local light probes And Alpha < 1 do Evaluate local light probe
-//        a = saturate(localLightProbe.a - Alpha) RGB += localLightProbe.rgb * a
-//        Alpha = saturate(a + Alpha)
-//        // Large range reflections
-//        If Alpha < 1 Then
-//        Evaluate distant light probe
-//        RGB += distantLightProbe.rgb * (1-Alpha)
-//    
-//}
+vec3 reflectionDirectionForLocalLightProbe(vec4 worldSpacePosition, vec3 worldSpaceReflectionDirection, mat4 cubeMapVolumeWorldToLocal, vec3 cubeMapPositionWS, float linearRoughness,
+                                           out float distIntersectionToShadedPoint,
+                                           out float distIntersectionToProbeCentre) {
+    vec3 rayLS = mat3(cubeMapVolumeWorldToLocal) * worldSpaceReflectionDirection;
+    vec3 positionLS = (cubeMapVolumeWorldToLocal * worldSpacePosition).xyz;
+    
+    vec3 unitary = vec3(1.f);
+    vec3 firstPlaneIntersect = (unitary - positionLS) / rayLS;
+    vec3 secondPlaneIntersect = (-unitary - positionLS) / rayLS;
+    
+    vec3 furthestPlane = max(firstPlaneIntersect, secondPlaneIntersect);
+    float dist = min(furthestPlane.x, min(furthestPlane.y, furthestPlane.z));
+    
+    vec3 intersectPositionWS = worldSpacePosition.xyz + worldSpaceReflectionDirection * dist;
+    vec3 reflectionDirectionWS = intersectPositionWS - cubeMapPositionWS.xyz;
+    
+    distIntersectionToShadedPoint = dist;
+    distIntersectionToProbeCentre = distance(intersectPositionWS, cubeMapPositionWS.xyz);
+    
+    return mix(reflectionDirectionWS, worldSpaceReflectionDirection, linearRoughness);
+}
+
+float computeDistanceBasedLinearRoughness(
+                                   float distIntersectionToShadedPoint,
+                                   float distIntersectionToProbeCenter,
+                                   float linearRoughness) {
+    // To avoid artifacts we clamp to the original linearRoughness
+    // which introduces an acceptable bias and allows conservation
+    // of mirror reflection behavior for a smooth surface.
+    float newLinearRoughness = clamp(distIntersectionToShadedPoint /
+                                       distIntersectionToProbeCenter * linearRoughness, 0, linearRoughness);
+    return mix(newLinearRoughness, linearRoughness, linearRoughness);
+}
+
+layout(std140) struct LightProbe {
+    mat4 boundingVolumeWorldToLocal;
+    vec4 cubeMapPosition;
+    int isEnvironmentMap;
+    int padding;
+    int padding2;
+    int padding3;
+};
+
+vec4 evaluateLightProbe(vec4 worldSpacePosition, vec3 N, vec3 V, float NdotV, vec3 R, MaterialRenderingData material, LightProbe lightProbe) {
+    
+    float roughness;
+    float linearRoughness;
+    
+    if (lightProbe.isEnvironmentMap != 0) {
+        float distIntersectionToShadedPoint;
+        float distIntersectionToProbeCentre;
+        R = reflectionDirectionForLocalLightProbe(worldSpacePosition, R, lightProbe.boundingVolumeWorldToLocal, lightProbe.cubeMapPosition.xyz, material.linearRoughness,
+                                                  distIntersectionToShadedPoint,
+                                                  distIntersectionToProbeCentre);
+        linearRoughness = computeDistanceBasedLinearRoughness(distIntersectionToShadedPoint, distIntersectionToProbeCentre, material.linearRoughness);
+        roughness = linearRoughness * linearRoughness;
+    } else {
+        roughness = material.roughness;
+        linearRoughness = material.linearRoughness;
+    }
+        
+    vec3 diffuseResult = evaluateIBLDiffuse(N, V, NdotV, material.roughness).rgb * material.albedo;
+    vec4 specularResult = evaluateIBLSpecular(N, R, NdotV, material.linearRoughness, material.roughness, material.f0, material.f90);
+    return vec4(diffuseResult + specularResult.rgb, specularResult.a);
+}
+
+#define MaxLightProbeCount 64
+
+uniform LightProbe lightProbes[MaxLightProbeCount];
+uniform int lightProbeIndices[MaxLightProbeCount + 1];
+
+vec3 evaluateIBL(vec4 worldSpacePosition, vec3 N, vec3 V, float NdotV, vec3 R, MaterialRenderingData material) {
+    vec4 total = vec4(0);
+    
+    int lightProbeCount = lightProbeIndices[0];
+    // Medium and long range reflections
+    for (int i = 0; i < lightProbeCount && total.a < 1.0; i++) {
+        LightProbe probe = lightProbes[i + 1];
+        
+        vec4 evaluationResult = evaluateLightProbe(worldSpacePosition, N, V, NdotV, R, material, probe);
+        float alpha = saturate(evaluationResult.a - total.a);
+        total.rgb += evaluationResult.rgb;
+        total.a = saturate(alpha + total.a);
+    }
+    
+    return total.rgb;
+}
 
